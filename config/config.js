e4766676711b00f64c30349e8fa824d6a5472355
Alberto Pardo Díaz <albertopardo21690@gmail.com>
2019-07-10 23:08:10 +0200

    Autenticación con Google entre otras cosas

--- config/config.js
+++ config/config.js
@@ -1,1 +1,5 @@
 module.exports.SEED = 'este-es-un-seed-dificil';
+
+// Google
+module.exports.GOOGLE_CLIENT_ID = '681141320644-3hcccd6ai3pjshkt6uhhq30lqqkjqtio.apps.googleusercontent.com';
+module.exports.GOOGLE_SECRET = '_anThiRi7eZNrQ4CzZUYXWT6';
